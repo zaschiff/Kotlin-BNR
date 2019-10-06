fd1f9e0d7dbe756390f7af3bc9d3ce50862b5c1b
Zach Schiff <zaschiff@gmail.com>
2019-10-06 09:51:16 -0600

    Updating TypIntro.kt

--- Sandbox/src/TypeIntro.kt
+++ Sandbox/src/TypeIntro.kt
@@ -1,3 +1,12 @@
+/*
+**  Application: Sandbox
+**  Author: Zach Schiff
+**  Date: 10/05/2019
+**  Version: 1.1
+**  Description: This is a simple sandbox app to learn kotlin code from
+**  the training book Kotlin Programming: The Big Nerd Ranch Guide
+ */
+
 const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
