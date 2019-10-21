05706ee6da301e3eb958d34b9088207afb57b5ca
Zach Schiff <zaschiff@gmail.com>
2019-10-20 18:31:09 -0600

    Completed chapter 13 Challenges

---
+++ Challenge13RiddleofExcalibur/src/RiddleofExcalibur.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val sword = Sword("Excalibur")
+    println(sword.name)
+    sword.name = "Gleipnir"
+    println(sword.name)
+}
---
+++ Challenge13RiddleofExcalibur/src/Sword.kt
@@ -0,0 +1,11 @@
+class Sword (_name: String) {
+    var name = _name
+        get() = "The Legendary $field"
+        set(value) {
+            field = value.toLowerCase().reversed().capitalize()
+        }
+
+    init {
+        name = _name
+    }
+}
